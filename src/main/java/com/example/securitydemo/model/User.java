b9171419969b8c61cc3d1078983fdd7ba46dd815
lukasze <wyspianski.lukasz@gmail.com>
2022-01-16 07:00:31 +0100

    Rozwiązanie

---
+++ src/main/java/com/example/securitydemo/model/User.java
@@ -0,0 +1,28 @@
+package com.example.securitydemo.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Username is required.")
+    private String username;
+
+    @NotEmpty(message = "Email is required.")
+    private String email;
+
+    private LocalDateTime created = LocalDateTime.now();
+
+}
